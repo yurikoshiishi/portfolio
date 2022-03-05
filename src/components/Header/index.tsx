1d8a17a56ea0fb14521a766fadf5233bbad976a6
Yuri Koshiishi <55642841+yurikoshiishi@users.noreply.github.com>
2022-03-05 23:54:21 +0900

    add: lang select

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,27 +2,66 @@ import {
   Box,
   Flex,
   Button,
-  Stack,
   useColorMode,
   Container,
+  Menu,
+  MenuButton,
+  MenuList,
+  Text,
+  MenuOptionGroup,
+  MenuItemOption,
 } from "@chakra-ui/react";
-import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { ChevronDownIcon, MoonIcon, SunIcon } from "@chakra-ui/icons";
+import useTranslation from "next-translate/useTranslation";
+import i18nConfig from "../../../i18n";
 
 interface HeaderProps {}
 
+const localeToLangMap: { [key: string]: string } = {
+  en: "English",
+  ja: "日本語",
+};
+
 const Header: React.VFC<HeaderProps> = () => {
+  const { lang } = useTranslation();
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
     <Container maxWidth="container.xl">
       <Flex h={16} alignItems="center" justifyContent="space-between">
-        <Box></Box>
+        <Box>
+          <Menu>
+            <MenuButton
+              as={Button}
+              variant="outline"
+              cursor="pointer"
+              minW={0}
+              rightIcon={<ChevronDownIcon />}
+            >
+              <Text color="gray.200" fontSize="sm">
+                Language: {localeToLangMap[lang]}
+              </Text>
+            </MenuButton>
+            <MenuList>
+              <MenuOptionGroup value={lang}>
+                {i18nConfig.locales.map((locale) => (
+                  <MenuItemOption
+                    value={locale}
+                    key={locale}
+                    as="a"
+                    href={`/${locale}`}
+                  >
+                    {localeToLangMap[locale]}
+                  </MenuItemOption>
+                ))}
+              </MenuOptionGroup>
+            </MenuList>
+          </Menu>
+        </Box>
         <Flex alignItems="center">
-          <Stack direction="row" spacing={7}>
-            <Button onClick={toggleColorMode}>
-              {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
-            </Button>
-          </Stack>
+          <Button onClick={toggleColorMode}>
+            {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+          </Button>
         </Flex>
       </Flex>
     </Container>
