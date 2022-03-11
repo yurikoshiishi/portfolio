01808ff597901834e5b4a639170ac6383ad51820
Yuri Koshiishi <55642841+yurikoshiishi@users.noreply.github.com>
2022-03-11 23:44:31 +0900

    modify: color mode into theme config

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import { ColorModeScript } from "@chakra-ui/react";
+import { ColorModeScript, theme } from "@chakra-ui/react";
 
 export default class MyDocument extends Document {
   render() {
@@ -13,7 +13,7 @@ export default class MyDocument extends Document {
           <link rel="manifest" href="/manifest.json" />
         </Head>
         <body>
-          <ColorModeScript initialColorMode="system" />
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
           <NextScript />
         </body>
--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -1,8 +1,14 @@
-import { extendTheme } from "@chakra-ui/react";
+import { extendTheme, ThemeConfig } from "@chakra-ui/react";
 
 export const googleFonts: string[] = ["Montserrat", "Noto Sans JP"];
 
+const config: ThemeConfig = {
+  initialColorMode: "system",
+  useSystemColorMode: true,
+};
+
 export const theme = extendTheme({
+  config,
   fonts: {
     heading: `"Montserrat", "Noto Sans JP", -apple-system, "Helvetica Neue",
     Arial, "Hiragino Kaku Gothic ProN", "Hiragino Sans", Meiryo, sans-serif`,
