4925b34d0199e3559ad2ca9b40863b6cd777d600
Yuri Koshiishi <55642841+yurikoshiishi@users.noreply.github.com>
2022-03-12 18:29:49 +0900

    modify: bg override to fix bug in safari

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, useColorMode } from "@chakra-ui/react";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import { useFontLoaded } from "../hooks/useFontLoaded";
@@ -11,6 +11,7 @@ import { BASE_URL } from "../constants";
 
 const IndexPage = () => {
   const isFontLoaded = useFontLoaded(googleFonts);
+  const { colorMode } = useColorMode();
 
   if (!isFontLoaded) {
     return null;
@@ -21,13 +22,22 @@ const IndexPage = () => {
       <Head>
         <link rel="canonical" href={`${BASE_URL}/`} />
       </Head>
-      <Flex height="100%" direction="column" alignItems="center">
-        <Header />
-        <Box flex={1}>
-          <Hero links={socialLinks} />
-        </Box>
-      </Flex>
-      <Background icons={technologyIcons} />
+      <Box
+        height="100%"
+        position="relative"
+        zIndex={1}
+        // NOTE: setting bg here to override body bg, in order to prevent a bug in safari
+        // https://github.com/chakra-ui/chakra-ui/issues/5641
+        background={colorMode === "dark" ? "gray.800" : "white"}
+      >
+        <Flex height="100%" direction="column" alignItems="center">
+          <Header />
+          <Box flex={1}>
+            <Hero links={socialLinks} />
+          </Box>
+        </Flex>
+        <Background icons={technologyIcons} />
+      </Box>
     </>
   );
 };
