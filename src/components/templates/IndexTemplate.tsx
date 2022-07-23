b7d315c6029ba766a92cf823aa4a9dbb1a8fabfa
Yuri Koshiishi <55642841+yurikoshiishi@users.noreply.github.com>
2022-07-23 09:46:56 +0900

    refactor: index template

--- src/components/templates/IndexTemplate.tsx
+++ src/components/templates/IndexTemplate.tsx
@@ -1,11 +1,21 @@
+import Background from "@/components/Background";
+import { GAUNTLET_IMAGE_PATHS } from "@/components/Gauntlet";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import { BASE_URL } from "@/constants";
+import { socialLinks, technologyIcons } from "@/data";
+import { useFontLoaded } from "@/hooks/useFontLoaded";
+import { googleFonts } from "@/theme";
+import { Box, Flex, useColorMode } from "@chakra-ui/react";
 import Head from "next/head";
 import React from "react";
-import { BASE_URL } from "../../constants";
-import { GAUNTLET_IMAGE_PATHS } from "../Gauntlet";
 
 interface IndexTemplateProps {}
 
 const IndexTemplate: React.FC<IndexTemplateProps> = ({ children }) => {
+  const isFontLoaded = useFontLoaded(googleFonts);
+  const { colorMode } = useColorMode();
+
   return (
     <>
       <Head>
@@ -14,7 +24,24 @@ const IndexTemplate: React.FC<IndexTemplateProps> = ({ children }) => {
         ))}
         <link rel="canonical" href={`${BASE_URL}/`} />
       </Head>
-      {children}
+      <Box
+        height="100%"
+        position="relative"
+        zIndex={1}
+        // NOTE: setting bg here to override body bg, in order to prevent a bug in safari
+        // https://github.com/chakra-ui/chakra-ui/issues/5641
+        background={colorMode === "dark" ? "gray.800" : "white"}
+      >
+        {isFontLoaded && (
+          <Flex height="100%" direction="column" alignItems="center">
+            <Header />
+            <Box flex={1}>
+              <Hero links={socialLinks} />
+            </Box>
+          </Flex>
+        )}
+        <Background icons={technologyIcons} />
+      </Box>
     </>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,41 +1,5 @@
-import React from "react";
-import { Box, Flex, useColorMode } from "@chakra-ui/react";
-import Header from "../components/Header";
-import Hero from "../components/Hero";
-import { useFontLoaded } from "../hooks/useFontLoaded";
-import { googleFonts } from "../theme";
-import { socialLinks, technologyIcons } from "../data";
-import Background from "../components/Background";
 import IndexTemplate from "../components/templates/IndexTemplate";
 
-const IndexPage = () => {
-  const isFontLoaded = useFontLoaded(googleFonts);
-  const { colorMode } = useColorMode();
-
-  if (!isFontLoaded) {
-    return <IndexTemplate />;
-  }
-
-  return (
-    <IndexTemplate>
-      <Box
-        height="100%"
-        position="relative"
-        zIndex={1}
-        // NOTE: setting bg here to override body bg, in order to prevent a bug in safari
-        // https://github.com/chakra-ui/chakra-ui/issues/5641
-        background={colorMode === "dark" ? "gray.800" : "white"}
-      >
-        <Flex height="100%" direction="column" alignItems="center">
-          <Header />
-          <Box flex={1}>
-            <Hero links={socialLinks} />
-          </Box>
-        </Flex>
-        <Background icons={technologyIcons} />
-      </Box>
-    </IndexTemplate>
-  );
-};
-
-export default IndexPage;
+export default function Page() {
+  return <IndexTemplate />;
+}
