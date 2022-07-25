1e2ea4685783b306cf075d6403a8799ed147780a
Yuri Koshiishi <55642841+yurikoshiishi@users.noreply.github.com>
2022-07-25 09:57:33 +0900

    fix: viewport meta

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,11 +5,15 @@ import "../styles/index.css";
 
 import { ColorModeProvider } from "@/contexts/color-mode";
 import { LanguageProvider } from "@/contexts/language";
+import Head from "next/head";
 
 function App({ Component, pageProps }: any) {
   return (
     <LanguageProvider>
       <ColorModeProvider>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
         <Component {...pageProps} />
       </ColorModeProvider>
     </LanguageProvider>
