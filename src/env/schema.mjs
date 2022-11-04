20c48e571d365040778b4b6e30f6c99d8582f456
Yuri Koshiishi <55642841+yurikoshiishi@users.noreply.github.com>
2022-11-04 19:41:48 +0900

    feat: contentful env

---
+++ src/env/schema.mjs
@@ -0,0 +1,27 @@
+// @ts-check
+import { string, z } from "zod";
+
+/**
+ * Specify your server-side environment variables schema here.
+ * This way you can ensure the app isn't built with invalid env vars.
+ */
+export const serverSchema = z.object({
+  CONTENTFUL_SPACE_ID: string(),
+  CONTENTFUL_DELIVERY_API_KEY: string(),
+  CONTENTFUL_PREVIEW_API_KEY: string(),
+});
+
+/**
+ * Specify your client-side environment variables schema here.
+ * This way you can ensure the app isn't built with invalid env vars.
+ * To expose them to the client, prefix them with `NEXT_PUBLIC_`.
+ */
+export const clientSchema = z.object({});
+
+/**
+ * You can't destruct `process.env` as a regular object, so you have to do
+ * it manually here. This is because Next.js evaluates this at build time,
+ * and only used environment variables are included in the build.
+ * @type {{ [k in keyof z.infer<typeof clientSchema>]: z.infer<typeof clientSchema>[k] | undefined }}
+ */
+export const clientEnv = {};
