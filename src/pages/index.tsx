ca744c25217e93d84eb62940bf1ac20dfc978ca4
Yuri Koshiishi <55642841+yurikoshiishi@users.noreply.github.com>
2023-09-03 00:31:54 +0900

    refactor: remove template layer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,42 @@
-import { LANGUAGES } from "@/constants";
-import { iconNames } from "@/data";
+import Background from "@/components/Background";
+import { GAUNTLET_IMAGE_PATHS } from "@/components/Gauntlet";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import StaticContent from "@/components/StaticContent";
+import { BASE_URL, LANGUAGES } from "@/constants";
+import { iconNames, socialLinks } from "@/data";
 import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import IndexTemplate from "../components/templates/IndexTemplate";
+import Head from "next/head";
 
 export default function Page(
   props: InferGetStaticPropsType<typeof getStaticProps>
 ) {
-  return <IndexTemplate {...props} />;
+  return (
+    <>
+      <Head>
+        <title>Yuri Koshiishi | Portfolio</title>
+        <meta
+          name="description"
+          content="Thank you for visiting Yuri Koshiishi Portfolio. Please feel free to contact me anytime."
+        />
+        <link rel="preload" href="/assets/avatar.jpg" as="image" />
+        <link rel="preload" href={GAUNTLET_IMAGE_PATHS.idle} as="image" />
+        <link rel="canonical" href={`${BASE_URL}/`} />
+      </Head>
+      <div className="h-full relative z-[1]">
+        <div className="h-full flex flex-col items-center">
+          <Header />
+          <div className="flex-1">
+            <Hero links={socialLinks} />
+          </div>
+        </div>
+      </div>
+      <StaticContent>
+        <Background iconNames={props.iconNames} />
+      </StaticContent>
+    </>
+  );
 }
 
 export const getStaticProps = async ({ locale }: GetStaticPropsContext) => {
